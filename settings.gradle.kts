aab0b329ba2ab354e152226bfc73397ada1b089c
unknown <adeliacesar4@gmail.com>
2023-12-18 16:17:58 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "percobaan 9 API_F55122004"
+include(":app")
